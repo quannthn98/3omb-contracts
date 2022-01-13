c31827d70f414a6f42e5bc2ef37bedfd4eb114cb
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2022-01-12 20:21:06 -0800

    update repository

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-waffle")
+module.exports = {
+    networks: {
+        hardhat: {},
+        ropsten: {
+            url: "https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+            accounts: []
+        }
+    },
+    solidity: {
+        compilers: [{
+            version: "0.6.12",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                }
+            }
+        }, {
+            version: "0.8.7",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                }
+            }
+        }]
+    }
+}
