cbc6abc3a3a7b20a1c886aa31eef734795a6e5cc
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2022-01-13 09:38:50 -0800

    fix tomb amount

--- contracts/BondTreasury.sol
+++ contracts/BondTreasury.sol
@@ -210,9 +210,10 @@ contract BondTreasury is Ownable {
     // Calculate Tomb return of bonding amount of token
 
     function getTombReturn(address token, uint256 amount) public view onlyAsset(token) returns (uint256) {
+        uint256 tombPrice = getTombPrice();
         uint256 tokenPrice = getTokenPrice(token);
         uint256 bondPremium = getBondPremium();
-        return amount * tokenPrice * (bondPremium + DENOMINATOR) * assets[token].multiplier / (DENOMINATOR * DENOMINATOR);
+        return amount * tokenPrice * (bondPremium + DENOMINATOR) * assets[token].multiplier / (DENOMINATOR * DENOMINATOR) / tombPrice;
     }
 
     // Calculate premium for bonds based on bonding curve
